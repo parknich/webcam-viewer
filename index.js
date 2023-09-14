988abaf393c9096d8874fbd985cdb991bcfa3505
parknich <45834520+parknich@users.noreply.github.com>
2023-09-14 12:31:54 -0400

    zdsvsDA

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var vid = document.querySelector("#videoElement");
+const wrapper = document.getElementById("wrapper");
 const errorField = document.getElementById("errorField");
 const selectBox = document.getElementById("selectBox");
 const submitButton = document.getElementById("submitButton");
@@ -8,57 +9,62 @@ let resMinFps;
 let resMaxFps;
 let res = (submitButton.onclick = async () => {
   res = selectBox.value;
+  switch (res) {
+    case 0:
+      resWidth = 1920;
+      resHeight = 1080;
+      resMinFps = 55;
+      resMaxFps = 60;
+      break;
+    case 1:
+      resWidth = 1920;
+      resHeight = 1080;
+      resMinFps = 25;
+      resMaxFps = 30;
+      break;
+    case 2:
+      resWidth = 1280;
+      resHeight = 720;
+      resMinFps = 55;
+      resMaxFps = 60;
+      break;
+    case 3:
+      resWidth = 1280;
+      resHeight = 720;
+      resMinFps = 25;
+      resMaxFps = 30;
+      break;
+  
+    default:
+      console.error("WebCamViewer: Unknown Error");
+      break;
+  }
+  
 });
 
-switch (res) {
-  case 0:
-    resWidth = 1920;
-    resHeight = 1080;
-    resMinFps = 55;
-    resMaxFps = 60;
-    break;
-  case 1:
-    resWidth = 1920;
-    resHeight = 1080;
-    resMinFps = 25;
-    resMaxFps = 30;
-    break;
-  case 2:
-    resWidth = 1280;
-    resHeight = 720;
-    resMinFps = 55;
-    resMaxFps = 60;
-    break;
-  case 3:
-    resWidth = 1280;
-    resHeight = 720;
-    resMinFps = 25;
-    resMaxFps = 30;
-    break;
 
-  default:
-    console.error("WebCamViewer: Unknown Error");
-    break;
-}
-vid.style.width = resWidth
-vid.style.height = resHeight
 // Set constraints for the video element, including setting the FPS to 55-60
-const constraints = {
-  video: {
-    video: true,
-    audio: true,
-    controls: true,
-    autoplay: true,
-    frameRate: { ideal: resMinFps, max: resMaxFps },
-  },
-};
+
 
 // Set the video element to the camera
 function streamFunc() {
+  const constraints = {
+    video: {
+      video: true,
+      audio: true,
+      controls: true,
+      autoplay: true,
+      frameRate: { ideal: resMinFps, max: resMaxFps },
+    },
+  };
   if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices
       .getUserMedia(constraints)
       .then(function (stream) {
+        vid.style.width = resWidth
+        vid.style.height = resHeight
+        wrapper.style.width = resWidth;
+        wrapper.style.height = resHeight;
         vid.srcObject = stream;
       })
       .catch(function (err0r) {
@@ -68,4 +74,3 @@ function streamFunc() {
       });
   }
 }
-streamFunc();
