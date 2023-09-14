0edbc391f3fa87ac733ba2f13284af160bd91a32
parknich <45834520+parknich@users.noreply.github.com>
2023-09-14 12:24:21 -0400

    sfzACFv

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 var vid = document.querySelector("#videoElement");
 const errorField = document.getElementById("errorField");
+const selectBox = document.getElementById("selectBox");
+const submitButton = document.getElementById("submitButton");
+let resWidth;
+let resHeight;
+let resMinFps;
+let resMaxFps;
+let res = (submitButton.onclick = async () => {
+  res = selectBox.value;
+});
 
+switch (res) {
+  case 0:
+    resWidth = 1920;
+    resHeight = 1080;
+    resMinFps = 55;
+    resMaxFps = 60;
+    break;
+  case 1:
+    resWidth = 1920;
+    resHeight = 1080;
+    resMinFps = 25;
+    resMaxFps = 30;
+    break;
+  case 2:
+    resWidth = 1280;
+    resHeight = 720;
+    resMinFps = 55;
+    resMaxFps = 60;
+    break;
+  case 3:
+    resWidth = 1280;
+    resHeight = 720;
+    resMinFps = 25;
+    resMaxFps = 30;
+    break;
+
+  default:
+    console.error("WebCamViewer: Unknown Error");
+    break;
+}
+vid.style.width = resWidth
+vid.style.height = resHeight
 // Set constraints for the video element, including setting the FPS to 55-60
 const constraints = {
-  video:
-  {
+  video: {
     video: true,
     audio: true,
     controls: true,
     autoplay: true,
-    frameRate: { ideal: 55, max: 60 }
-  }
+    frameRate: { ideal: resMinFps, max: resMaxFps },
+  },
 };
 
 // Set the video element to the camera
@@ -22,7 +62,9 @@ function streamFunc() {
         vid.srcObject = stream;
       })
       .catch(function (err0r) {
-        console.error("Something went wrong when trying to set video element, trying again");
+        console.error(
+          "Something went wrong when trying to set video element, trying again"
+        );
       });
   }
 }
